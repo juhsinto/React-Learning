d5c673666a2b0024a21c7eb1d5ac1ebab44a0a11
Jacinto Mendes <jmz093@gmail.com>
2019-08-12 08:29:29 +1000

    added commentry for 03

--- 03 - components/src/index.js
+++ 03 - components/src/index.js
@@ -4,6 +4,8 @@ import faker from 'faker';
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
 
+// data is being passed as a prop from here to the CommentDetail component
+
 const App = () => {
 	return (
 		<div className="ui container comments">
@@ -11,15 +13,17 @@ const App = () => {
 				<CommentDetail author="Dickens" text="blahblah" srcImage={faker.image.avatar()}/>
 			</ApprovalCard>
 			
-			<ApprovalCard>
-				<CommentDetail author="cat" text="idiot" srcImage={faker.image.avatar()}/>
-			</ApprovalCard>
-
-			<ApprovalCard>
-				<CommentDetail author="rat" text="super" srcImage={faker.image.avatar()}/>
-			</ApprovalCard>
 		</div>
 	);
+
+
+			// <ApprovalCard>
+			// 	<CommentDetail author="cat" text="idiot" srcImage={faker.image.avatar()}/>
+			// </ApprovalCard>
+
+			// <ApprovalCard>
+			// 	<CommentDetail author="rat" text="super" srcImage={faker.image.avatar()}/>
+			// </ApprovalCard>
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
