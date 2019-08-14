7fe5e77981a06c724c22797703363262a68a189e
Jacinto Mendes <jmz093@gmail.com>
2019-08-15 08:37:06 +1000

    finished 175

--- 09 - blog/src/reducers/postsReducer.js
+++ 09 - blog/src/reducers/postsReducer.js
@@ -1,5 +1,12 @@
 export default (state = [], action => {
 	// return 'adfas'
+	
+	// if (action.type === 'FETCH_POSTS') {
+	// 	return action.paylod;
+	// } 
+
+	// return state;
+
 	switch(state) {
 		case 'FETCH_POSTS':
 			return action.payload;
