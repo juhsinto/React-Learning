995a5689b2bb6a05ba4bfaf2ae31c32573824558
Jacinto Mendes <jmz093@gmail.com>
2019-08-15 08:32:52 +1000

    166: added thunk

--- 09 - blog/src/actions/index.js
+++ 09 - blog/src/actions/index.js
@@ -1,7 +1,7 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 
-export const fetchPosts = async () => {
+export const fetchPosts = () => {
 	return async function(dispatch, getState) {
 		const response = await jsonPlaceholder.get('/posts');
 
