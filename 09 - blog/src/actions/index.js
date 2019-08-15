44e4f2b0f087edb5984992c3a3557877f638d2fe
Jacinto Mendes <jmz093@gmail.com>
2019-08-16 08:05:22 +1000

    finished 179

--- 09 - blog/src/actions/index.js
+++ 09 - blog/src/actions/index.js
@@ -20,3 +20,9 @@ export const fetchPosts = () => async dispatch => {
 	const response = await jsonPlaceholder.get('/posts');
 	dispatch({type: 'FETCH_POSTS', payload: response.data});
 };
+
+export const fetchUser = (userId) => async dispatch => {
+	// const resonse = await jsonPlaceholder.get('/users/' + userId);
+	const resonse = await jsonPlaceholder.get(`/users/${userId}`);
+	dispatch({type: 'FETCH_USER', payload: response.data})
+};
