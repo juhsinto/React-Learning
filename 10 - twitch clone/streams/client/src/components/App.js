9fcde55b79218f91bd8139d4de006e240b37d459
Jacinto Mendes <jmz093@gmail.com>
2019-08-20 08:17:54 +1000

    finished 206 - links inside routers

--- 10 - twitch clone/streams/client/src/components/App.js
+++ 10 - twitch clone/streams/client/src/components/App.js
@@ -1,33 +1,30 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import StreamList from './streams/StreamList';
 import StreamCreate from './streams/StreamCreate';
 import StreamShow from './streams/StreamShow';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
+import Header from './Header'
 
 
 
-const PageOne = () => {
-	return (<div> PageOne  <Link to="/pagetwo"> page two </Link> </div>);
-};
-
-const PageTwo = () => {
-	return (<div> PageTwo  <Link to="/"> page one </Link></div> );
-};
 
 const App = () => {
-	return (<div> 
-		<BrowserRouter>
-			<div>
-				<Route path="/" exact component={StreamList} />
-				<Route path="/streams/new" exact component={StreamCreate} />
-				<Route path="/streams/edit" exact component={StreamEdit} />
-				<Route path="/streams/delete" exact component={StreamDelete} />
-				<Route path="/streams/show" exact component={StreamShow} />
-			</div>
-		</BrowserRouter> 
-	</div>);
+	return (
+		<div className="ui container" > 
+			<BrowserRouter>
+				<Header />
+				<div>
+					<Route path="/" exact component={StreamList} />
+					<Route path="/streams/new" exact component={StreamCreate} />
+					<Route path="/streams/edit" exact component={StreamEdit} />
+					<Route path="/streams/delete" exact component={StreamDelete} />
+					<Route path="/streams/show" exact component={StreamShow} />
+				</div>
+			</BrowserRouter> 
+		</div>
+	);
 }
 
 export default App;
