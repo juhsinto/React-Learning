8b20fe44aad96785967d745bf1c2033dfd753c18
Jacinto Mendes <jmz093@gmail.com>
2019-08-20 08:03:44 +1000

    finished 203 - wiring up routes

--- 10 - twitch clone/streams/client/src/components/App.js
+++ 10 - twitch clone/streams/client/src/components/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamShow from './streams/StreamShow';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+
+
 
 const PageOne = () => {
 	return (<div> PageOne  <Link to="/pagetwo"> page two </Link> </div>);
@@ -13,8 +20,11 @@ const App = () => {
 	return (<div> 
 		<BrowserRouter>
 			<div>
-			<Route path="/" exact component={PageOne} />
-			<Route path="/pagetwo"  component={PageTwo} />
+				<Route path="/" exact component={StreamList} />
+				<Route path="/streams/new" exact component={StreamCreate} />
+				<Route path="/streams/edit" exact component={StreamEdit} />
+				<Route path="/streams/delete" exact component={StreamDelete} />
+				<Route path="/streams/show" exact component={StreamShow} />
 			</div>
 		</BrowserRouter> 
 	</div>);
