074307436a6b908e6d0247be56d18dc234c25931
Jacinto Mendes <jmz093@gmail.com>
2019-08-22 08:33:58 +1000

    added sign in/out buttons

--- 10 - twitch clone/streams/client/src/components/GoogleAuth.js
+++ 10 - twitch clone/streams/client/src/components/GoogleAuth.js
@@ -23,18 +23,27 @@ class GoogleAuth extends React.Component {
 		this.setState({isSignedIn: this.auth.isSignedIn.get()});
 	};
 
+	onSignIn = () => {
+		this.auth.signIn();
+	};
+
+	onSignOut = () => {
+		this.auth.signOut();
+	};
+
 	renderAuthButton() {
 		if (this.state.isSignedIn === null) {
-			return <div> I dont know if we are signed in </div>;
+			return null;
 		} else if (this.state.isSignedIn) {
-			return <div> I am signed in. </div>;
+			// NOT onClick={this.onSignOut()} because - then it would execute as soon as rendered
+			return <button onClick={this.onSignOut} className="ui red google button"> <i className="google icon"> </i>Sign Out </button>;
 		} else {
-			return <div> I am not signed in </div>;
+			return <button onClick={this.onSignIn} className="ui red google button"> <i className="google icon"> </i>Sign In with Google </button>;
 		}
 	};
 
 	render() {
-		return <div> Google Auth {this.renderAuthButton()}</div>;
+		return <div> {this.renderAuthButton()}</div>;
 	};
 }
 
