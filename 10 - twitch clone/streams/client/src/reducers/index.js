0b9ba452432170b04efbc5728becfd4c381f4e75
Jacinto Mendes <jmz093@gmail.com>
2019-08-24 19:33:16 +1000

    finished 228 - added redux-form

--- 10 - twitch clone/streams/client/src/reducers/index.js
+++ 10 - twitch clone/streams/client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux';
+import {reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({
 	// replaceMe: () => 5 //placeholder reducer!
-	auth: authReducer
+	auth: authReducer,
+	form: formReducer
 });
