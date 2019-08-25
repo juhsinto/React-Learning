5b9823458b1c29fdf430e41897dcf5f0fe352a1b
Jacinto Mendes <jmz093@gmail.com>
2019-08-26 08:26:19 +1000

    finished 240 - added axios, action creator to connect to jsonserver ; todo connect actioncreator to streamcreate component

--- 10 - twitch clone/streams/client/src/actions/index.js
+++ 10 - twitch clone/streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import {SIGN_IN, SIGN_OUT} from '../actions/types';
+import streams from '../apis/streams.js';
 
 export const signIn = (userId) => {
 	return {
@@ -11,4 +12,10 @@ export const signOut = () => {
 	return {
 		type: SIGN_OUT
 	};
+};
+
+export const createStream = (formValues) => {
+	return async (dispatch) => {
+		streams.post('/streams', formValues);
+	}
 };
