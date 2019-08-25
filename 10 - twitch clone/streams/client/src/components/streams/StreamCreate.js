51a23e319fab0c55ee0f9ff04bd97e4cca91e977
Jacinto Mendes <jmz093@gmail.com>
2019-08-26 08:35:42 +1000

    finished 241 - connected actionCreator to streamCreate component

--- 10 - twitch clone/streams/client/src/components/streams/StreamCreate.js
+++ 10 - twitch clone/streams/client/src/components/streams/StreamCreate.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Field, reduxForm} from 'redux-form';
-
+import {connect} from 'react-redux';
+import {createStream} from '../../actions';
 
 class StreamCreate extends React.Component {
 
@@ -30,8 +31,9 @@ class StreamCreate extends React.Component {
 			</div>);
 	}
 
-	onSubmit(formValues) {
+	onSubmit = (formValues) => {
 		console.log(formValues);
+		this.props.createStream(formValues);
 	}
 
 
@@ -64,7 +66,9 @@ const validateValues = (formValues) => {
 	return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
 	form: 'streamCreate',
 	validate: validateValues
 })(StreamCreate);
+
+export default connect(null, {createStream})(formWrapped);
--- 10 - twitch clone/streams/client/src/index.js
+++ 10 - twitch clone/streams/client/src/index.js
@@ -6,6 +6,7 @@ import {Provider } from 'react-redux';
 import {createStore} from 'redux';
 
 import {applyMiddleware, compose} from 'redux';
+import reduxThunk from 'redux-thunk';
 
 
 import App from './components/App';
@@ -14,7 +15,7 @@ import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, 
-		composeEnhancers(applyMiddleware())
+		composeEnhancers(applyMiddleware(reduxThunk))
 	);
 
 ReactDOM.render(
