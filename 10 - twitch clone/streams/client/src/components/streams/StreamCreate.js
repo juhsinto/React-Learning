733f619f5057cec4cbe03696e16b464115dd7f07
Jacinto Mendes <jmz093@gmail.com>
2019-08-25 17:21:13 +1000

    finished 233 - added validation

--- 10 - twitch clone/streams/client/src/components/streams/StreamCreate.js
+++ 10 - twitch clone/streams/client/src/components/streams/StreamCreate.js
@@ -4,14 +4,18 @@ import {Field, reduxForm} from 'redux-form';
 
 class StreamCreate extends React.Component {
 
-	renderInput({input, label}) {
+	renderInput({input, label, meta}) {
 		// console.log(formProps);
 		// return <input onChange={formProps.input.onChange} 
 		// 			value={formProps.input.value} />
+
+		console.log(meta);
+
 		return (
 			<div className="field">
 				<label> {label} </label>
 				<input {...input} />
+				<div>{meta.error}</div>
 			</div>);
 	}
 
@@ -33,6 +37,23 @@ class StreamCreate extends React.Component {
 	}
 };
 
+const validateValues = (formValues) => {
+	const errors = {};
+
+	// if title wasnt entered
+	if (!formValues.title) {
+		errors.title = 'the title wasnt enetered';
+	}
+
+	// if description wasnt entered
+	if (!formValues.description) {
+		errors.description = 'the title wasnt enetered';
+	}
+
+	return errors;
+};
+
 export default reduxForm({
-	form: 'streamCreate'
+	form: 'streamCreate',
+	validate: validateValues
 })(StreamCreate);
