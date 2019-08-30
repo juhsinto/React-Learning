c0f6083aa427fddb0d8bc3d6a1e190104957af12
Jacinto Mendes <jmz093@gmail.com>
2019-08-30 14:24:12 +1000

    finished 268: getting data from props of parent component StreamList to child StreamEdit

--- 10 - twitch clone/streams/client/src/components/streams/StreamEdit.js
+++ 10 - twitch clone/streams/client/src/components/streams/StreamEdit.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {fetchStream} from '../../actions';
+import {fetchStream, editStream} from '../../actions';
+import StreamForm from './StreamForm';
 // const StreamEdit = (props) => {
 // 	console.log(props);
 // 	return <div>StreamEdit</div>;
@@ -12,11 +13,21 @@ class StreamEdit extends React.Component {
 		this.props.fetchStream(this.props.match.params.id);
 	}
 
+	onSubmit = (formValues) => {
+		console.log(formValues);
+	};
+
 	render() {
 		if (!this.props.stream) {
 			return <div>Loading </div>;
 		}
-		return <div>{this.props.stream.title}</div>;
+		return (
+				<div>
+					<h3>Edit a Stream</h3>
+					<StreamForm initialValues={this.props.stream}
+								onSubmit={this.onSubmit} />
+				</div>
+			);
 	}
 	
 
@@ -28,4 +39,4 @@ const mapStateToProps = (state, ownProps) => {
 	return {stream: state.streams[ownProps.match.params.id]}
 }
 
-export default connect(mapStateToProps, {fetchStream})(StreamEdit);
+export default connect(mapStateToProps, {fetchStream, editStream})(StreamEdit);
