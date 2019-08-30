4de34c461ba0192744f5fdb24daa9b39098807f6
Jacinto Mendes <jmz093@gmail.com>
2019-08-30 17:14:54 +1000

    finished 280: fetching stream in StreamDelete

--- 10 - twitch clone/streams/client/src/components/streams/StreamDelete.js
+++ 10 - twitch clone/streams/client/src/components/streams/StreamDelete.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import Modal from '../Modal';
+import {connect} from 'react-redux';
+import {fetchStream} from '../../actions';
 import history from '../../history';
 
-const StreamDelete = () => {
+class StreamDelete extends React.Component {
 
-	const actions = (
+	componentDidMount() {
+		this.props.fetchStream(this.props.match.params.id);
+	}
+
+	renderActions() {
+		return (
 			<React.Fragment>
 				<button className="ui button negative">Delete</button>
 				<button className="ui button">Cancel</button>
 			</React.Fragment>
 		);
+	}
 
-	return (
+	render() {
+		return (
 		<div>
 			StreamDelete <Modal title="Delete Stream"
-				actions={actions}
+				actions={this.renderActions()}
 				onDismiss={() => history.push('/')}
 				content="Are you sure you want to delete this stream?"/>
 		</div>
 		);
-};
+	}
+}
 
-export default StreamDelete;
+export default connect(null, {fetchStream})(StreamDelete);
