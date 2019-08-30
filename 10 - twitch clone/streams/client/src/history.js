35404fdadd5a00dcb8f36d88f5f4c1c36d4f41ce
Jacinto Mendes <jmz093@gmail.com>
2019-08-30 11:37:55 +1000

    added own history obj, and used Router instead of BrowserRouter

---
+++ 10 - twitch clone/streams/client/src/history.js
@@ -0,0 +1,7 @@
+// history is part of react-router-dom
+// import createHistory from 'history/createBrowserHistory';
+
+// export default createHistory();
+
+import { createBrowserHistory } from 'history'; 
+export default createBrowserHistory();
