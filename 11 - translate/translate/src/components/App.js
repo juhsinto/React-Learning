48be6060ef7ba07eddbaac858279a5c31d869731
Jacinto Mendes <jmz093@gmail.com>
2019-09-04 07:57:58 +1000

    finished 300: built the form, using a few components

--- 11 - translate/translate/src/components/App.js
+++ 11 - translate/translate/src/components/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import UserCreate from './UserCreate';
 
 class App extends React.Component {
+
+	state = {
+		language: 'English'
+	};
+
+	onLanguageChange = (language) => {
+		this.setState({language});
+	}
+
 	render() {
-		return <div className="ui container"> App </div>
+		return (
+			<div className="ui container">
+				<div>
+					Select a language:
+					<i className="flag us" onClick={() => this.onLanguageChange("English")}/>
+					<i className="flag nl" onClick={() => this.onLanguageChange("Dutch")}/>
+				</div>
+				<UserCreate />
+			</div>)
 	}
 }
 
