4dd074bfb14491588e16ecbffb9c467b610c3d16
Jacinto Mendes <jmz093@gmail.com>
2019-09-04 08:32:13 +1000

    finished 303: added logic for context, todo: hookup values to logic

--- 11 - translate/translate/src/components/Button.js
+++ 11 - translate/translate/src/components/Button.js
@@ -7,9 +7,10 @@ class Button extends React.Component {
 	static contextType = LanguageContext;
 	
 	render() {
-		console.log(this.context);
+		const text = this.context === 'English' ? 'Submit' : 'Vorleggen';
+
 		return (
-			<button className="ui button primary">Submit</button>
+			<button className="ui button primary">{text}</button>
 			);
 	}
 }
