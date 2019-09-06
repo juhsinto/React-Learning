14bb236599ec547c50f4acd63d62c05f529b05df
Jacinto Mendes <jmz093@gmail.com>
2019-09-06 11:26:08 +1000

    finished 306: getting data from Context, using the consumer

--- 11 - translate/translate/src/components/Button.js
+++ 11 - translate/translate/src/components/Button.js
@@ -3,14 +3,20 @@ import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
 
+	// only required when data should be assigned from Context's this.context property
+	// static contextType = LanguageContext;
 
-	static contextType = LanguageContext;
 	
 	render() {
-		const text = this.context === 'English' ? 'Submit' : 'Vorleggen';
+		// const text = this.context === 'English' ? 'Submit' : 'Vorleggen';
 
 		return (
-			<button className="ui button primary">{text}</button>
+			<button className="ui button primary">
+			<LanguageContext.Consumer>
+				{(value) => value === 'English' ? 'Submit' : 'Vorleggen'}
+			</LanguageContext.Consumer>
+
+			</button>
 			);
 	}
 }
