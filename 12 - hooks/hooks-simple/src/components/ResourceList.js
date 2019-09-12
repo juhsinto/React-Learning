35e8c1b0607f0399546a8443409caa0e110ad765
Jacinto Mendes <jmz093@gmail.com>
2019-09-13 09:52:32 +1000

    finished 330: added userList component that uses the useResource function

--- 12 - hooks/hooks-simple/src/components/ResourceList.js
+++ 12 - hooks/hooks-simple/src/components/ResourceList.js
@@ -1,32 +1,6 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React from 'react';
 
-
-const useResources = (resource) => {
-	const [resources, setResources] = useState([]);
-	
-	const fetchResource = async (resource) => {
-		const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-		// this.setState({resources: response.data});
-		setResources(response.data);
-	}
-
-	// you can add an argument (prevProps) to this method - so it is aware of the previous props
-	// to avoid a recurssive call
-	// async componentDidUpdate(prevProps) {
-	// 	if (prevProps.resource != this.props.resource) {
-	// 		const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-	// 		this.setState({resources: response.data});
-	// 	}
-	// }
-
-	// when using useEffect, make sure to pass the empty array, else it will behave recursively
-	useEffect(() => {
-		fetchResource(resource);
-	}, [resource])
-
-	return resources;
-}
+import useResources from './useResources';
 
 const ResourceList = ({resource}) => {
 
---
+++ 12 - hooks/hooks-simple/src/components/useResources.js
@@ -0,0 +1,30 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const useResources = (resource) => {
+	const [resources, setResources] = useState([]);
+	
+	const fetchResource = async (resource) => {
+		const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+		// this.setState({resources: response.data});
+		setResources(response.data);
+	}
+
+	// you can add an argument (prevProps) to this method - so it is aware of the previous props
+	// to avoid a recurssive call
+	// async componentDidUpdate(prevProps) {
+	// 	if (prevProps.resource != this.props.resource) {
+	// 		const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+	// 		this.setState({resources: response.data});
+	// 	}
+	// }
+
+	// when using useEffect, make sure to pass the empty array, else it will behave recursively
+	useEffect(() => {
+		fetchResource(resource);
+	}, [resource])
+
+	return resources;
+}
+
+export default useResources;
