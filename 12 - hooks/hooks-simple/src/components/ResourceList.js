bfb300072fb4f42cfecc4b1111ab9b8a8adb94e1
Jacinto Mendes <jmz093@gmail.com>
2019-09-13 09:47:21 +1000

    finished 322: added componentDidUpdate and logic to prevent recurssion

--- 12 - hooks/hooks-simple/src/components/ResourceList.js
+++ 12 - hooks/hooks-simple/src/components/ResourceList.js
@@ -10,6 +10,15 @@ class ResourceList extends React.Component {
 		this.setState({resources: response.data});
 	}
 
+	// you can add an argument (prevProps) to this method - so it is aware of the previous props
+	// to avoid a recurssive call
+	async componentDidUpdate(prevProps) {
+		if (prevProps.resource != this.props.resource) {
+			const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+			this.setState({resources: response.data});
+		}
+	}
+
 	render() {
 		return <div>{this.state.resources.length}</div>;
 	}
