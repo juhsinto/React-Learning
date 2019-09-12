9242b80b1dadef8cfce9ad91492a155c9e96f499
Jacinto Mendes <jmz093@gmail.com>
2019-09-13 09:48:36 +1000

    finished 323: initial refactor from class to functional component - for ResourceList

--- 12 - hooks/hooks-simple/src/components/ResourceList.js
+++ 12 - hooks/hooks-simple/src/components/ResourceList.js
@@ -1,27 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component {
+const ResourceList = () => {
 
-	state = { resources : []};
-
-	async componentDidMount() {
+	const [resources, setResources] = useState([]);
+	
+	const fetchResource = async () => {
 		const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
 		this.setState({resources: response.data});
 	}
 
 	// you can add an argument (prevProps) to this method - so it is aware of the previous props
 	// to avoid a recurssive call
-	async componentDidUpdate(prevProps) {
-		if (prevProps.resource != this.props.resource) {
-			const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-			this.setState({resources: response.data});
-		}
-	}
+	// async componentDidUpdate(prevProps) {
+	// 	if (prevProps.resource != this.props.resource) {
+	// 		const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+	// 		this.setState({resources: response.data});
+	// 	}
+	// }
 
-	render() {
-		return <div>{this.state.resources.length}</div>;
-	}
+	
+	return <div>{resources.length}</div>
+	
 }
 
 export default ResourceList;
